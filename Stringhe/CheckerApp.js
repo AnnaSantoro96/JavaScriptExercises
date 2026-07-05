028bbb23a7014843ef2bd3ca25e07ee19bd1495f
Anna Santoro <anna@MacBookPro.homenet.telecomitalia.it>
2026-07-05 15:16:09 +0200

    Esercizi Stringhe

---
+++ Stringhe/CheckerApp.js
@@ -0,0 +1,19 @@
+const hasDeveloperJob = true;
+
+if(hasDeveloperJob){
+    console.log("Tommy is employed as a developer");
+}
+
+const isTommyAGamer = true;
+
+if(isTommyAGamer){
+    console.log("Tommy loves to play world of Warcraft.");
+}
+
+const tommyAge = 16;
+
+if(tommyAge >= 16){
+    console.log("Tommy is old enough to drive.");
+}else{
+    console.log("Tommy is not old enough to drive.");
+}
